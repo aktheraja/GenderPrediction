d6d83b3c8d74069142bf0f41521c0577fec10509
aktheraja <wale.akin2007@gmail.com>
2019-08-19 21:40:10 -0600

    updated

---
+++ website/frontend/build/precache-manifest.b98ba8b97c76fe3e376ff730f81787de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220365b13cfbfd08808ba6b704cc5a6a",
+    "url": "/GenderPrediction/index.html"
+  },
+  {
+    "revision": "e0ef523f197693174a21",
+    "url": "/GenderPrediction/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "dca650a2b5805d55d03d",
+    "url": "/GenderPrediction/static/css/main.232305eb.chunk.css"
+  },
+  {
+    "revision": "e0ef523f197693174a21",
+    "url": "/GenderPrediction/static/js/2.073f37f5.chunk.js"
+  },
+  {
+    "revision": "dca650a2b5805d55d03d",
+    "url": "/GenderPrediction/static/js/main.b9e135ef.chunk.js"
+  },
+  {
+    "revision": "f54b159aa1d2dc343d99",
+    "url": "/GenderPrediction/static/js/runtime~main.1f411ef9.js"
+  }
+]);
